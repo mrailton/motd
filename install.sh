2fb551d7040e74f23bb2a0ab81da02555f64e8a9
Mark Railton <mark.railton@outlook.ie>
2025-03-18 13:57:42 +0000

    another tweak

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ curl -L https://github.com/mrailton/motd/archive/main.tar.gz | tar -zxv
 
 # Move old motd files to directory
 mkdir -p /etc/update-motd.d/old-motd
-mv /etc/update-motd.d/* /etc/update-motd.d/old-motd
+mv /etc/update-motd.d/* /etc/update-motd.d/old-motd/
 mv /etc/motd /etc/update-motd.d/old-motd
 
 # Move unzipped motd files to /etc
