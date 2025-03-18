ce8bf7e341a516572ba5710faed4550f31172ac9
Mark Railton <mark.railton@outlook.ie>
2025-03-18 13:57:06 +0000

    couple of tweaks

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ apt install update-notifier -y
 curl -L https://github.com/mrailton/motd/archive/main.tar.gz | tar -zxv
 
 # Move old motd files to directory
-mkdir -p etc/update-motd.d/old-motd
+mkdir -p /etc/update-motd.d/old-motd
 mv /etc/update-motd.d/* /etc/update-motd.d/old-motd
+mv /etc/motd /etc/update-motd.d/old-motd
 
 # Move unzipped motd files to /etc
 mv motd-main/motd/* /etc/update-motd.d
