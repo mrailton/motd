6076d67ef6d73a31ded97cd44762f1692172dd58
Mark Railton <mark.railton@outlook.ie>
2025-03-18 12:57:08 +0000

    update for my use

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
-# Spookdev-MOTD
-
-![Spookdev-MOTD](https://wizardlywonders.xyz/spookdev-motd-screenshot.png)
+# MOTD
 
 Custom MOTD scripts based off of PlexMOTD. This is running on Ubuntu, and the scripts are split into manageable chunks.
 
 ## Installation
-1. Run `curl -L https://raw.githubusercontent.com/NeonWizard/spookdev-motd/master/install.sh | sudo sh`.
+1. Run `curl -L https://raw.githubusercontent.com/mrailton/motd/main/install.sh | sudo sh`.
 2. Change `/etc/update-motd.d/colors.txt` to your liking.
 3. Optionally change `PrintLastLog` to `no` in `/etc/ssh/sshd_config`.
--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ apt install toilet -y
 apt install update-notifier -y
 
 # Download the archive
-curl -L https://github.com/NeonWizard/spookdev-motd/archive/master.tar.gz | tar -zxv
+curl -L https://github.com/mrailton/motd/archive/main.tar.gz | tar -zxv
 
 # Move old motd files to directory
-mkdir /etc/update-motd.d/old-motd
+mkdir -p etc/update-motd.d/old-motd
 mv /etc/update-motd.d/* /etc/update-motd.d/old-motd
 
 # Move unzipped motd files to /etc
-mv spookdev-motd-master/motd/* /etc/update-motd.d
+mv motd-main/motd/* /etc/update-motd.d
 
 # Clean up downloaded files
-rm -rf spookdev-motd-master
+rm -rf motd-main
